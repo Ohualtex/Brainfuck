6f1f46a64f7982ac36ffcd889f1dc38dac6c905a
Ohualtex <230626673+Ohualtex@users.noreply.github.com>
2026-08-28 18:33:42 +0300

    release: tag version 0.1.0 and finalize extension manifest

--- src/commands/runCode.ts
+++ src/commands/runCode.ts
@@ -44,7 +44,6 @@ export async function runBrainfuckCode() {
     userInput = input;
   }
 
-  vscode.window.setStatusBarMessage('Brainfuck programı çalıştırılıyor...', 2500);
   const channel = getOutputChannel();
   channel.show(true);
   channel.appendLine(`\n[${new Date().toLocaleTimeString()}] Brainfuck programı başlatılıyor...`);
